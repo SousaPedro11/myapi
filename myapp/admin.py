7533918d48c8090fc7bc1246918fa8d040fd8327
Pedro Sousa <ppls2106@gmail.com>
2020-05-25 12:29:06 -0300

    add Album, Member, Band no modelo com serializacao, autenticacao e interface no admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Music, Album, Band, Member
 
 # Register your models here.
+admin.site.register(Music)
+admin.site.register(Album)
+admin.site.register(Band)
+admin.site.register(Member)
--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Music(models.Model):
-	class Meta:
-		db_table = 'music'
+    class Meta:
+        db_table = 'music'
 
-	title = models.CharField(max_length=200)
-	seconds = models.IntegerField()
+    title = models.CharField(max_length=200)
+    seconds = models.IntegerField()
+    album = models.ForeignKey(
+        'Album', on_delete=models.PROTECT, related_name='musics')
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+    class Meta:
+        db_table = 'album'
+
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    band = models.ForeignKey(
+        'Band', on_delete=models.PROTECT, related_name='albuns')
+
+    def __str__(self):
+        return self.title
+
+
+class Band(models.Model):
+    class Meta:
+        db_table = 'band'
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    class Meta:
+        db_table = 'member'
+
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    band = models.ForeignKey(
+        'Band', on_delete=models.PROTECT, related_name='members')
+
+    def __str__(self):
+        return self.name
